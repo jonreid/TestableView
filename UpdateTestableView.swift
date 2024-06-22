e23e4f9eaf174ac5c81416b020a036dd696058c6
Jon Reid <jon@qualitycoding.org>
2024-06-22 08:42:45 -0700

    - b Import "YourModule" with Xcode replacement marker

--- UpdateTestableView.swift
+++ UpdateTestableView.swift
@@ -2,7 +2,7 @@
 // Copyright 2024 Jonathan M. Reid. https://github.com/jonreid/TestableView/blob/main/LICENSE.txt
 // SPDX-License-Identifier: MIT
 
-@testable import Mastermind
+@testable import <#YourModule#>
 import ViewInspector
 import XCTest
 
